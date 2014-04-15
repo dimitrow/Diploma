fb2ee03316d0d6a6733c735476aa028d37a88283
Eugene Dimitrow <e.dimitrow@gmail.com>
2014-04-16 00:59:40 +0400

    - page controller started
    Signed-off-by: Eugene Dimitrow <e.dimitrow@gmail.com>

---
+++ GalleryRootViewController.h
@@ -0,0 +1,14 @@
+//
+//  GalleryRootViewController.h
+//  CarRent
+//
+//  Created by Eugene Dimitrow on 4/15/14.
+//  Copyright (c) 2014 RockyTurtle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GalleryRootViewController : UIViewController
+- (IBAction)releaseGallery:(id)sender;
+
+@end
---
+++ GalleryRootViewController.m
@@ -0,0 +1,17 @@
+//
+//  GalleryRootViewController.m
+//  CarRent
+//
+//  Created by Eugene Dimitrow on 4/15/14.
+//  Copyright (c) 2014 RockyTurtle. All rights reserved.
+//
+
+#import "GalleryRootViewController.h"
+
+@implementation GalleryRootViewController
+
+- (IBAction)releaseGallery:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
